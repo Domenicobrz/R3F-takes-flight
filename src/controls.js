af9ffeadb50a5b831ab4a14ab8e1520b310d2d40
Domenicobrz <domenicobrz@gmail.com>
2023-07-26 13:41:55 +0200

    reset logic

--- src/controls.js
+++ src/controls.js
@@ -39,6 +39,15 @@ export function updatePlaneAxis(x, y, z, planePosition, camera) {
   if (controls["s"]) {
     pitchVelocity += 0.0025;
   }
+  if (controls["r"]) {
+    jawVelocity = 0;
+    pitchVelocity = 0;
+    turbo = 0;
+    x.set(1, 0, 0);
+    y.set(0, 1, 0);
+    z.set(0, 0, 1);
+    planePosition.set(0, 3, 7);
+  }
 
   x.applyAxisAngle(z, jawVelocity);
   y.applyAxisAngle(z, jawVelocity);
@@ -53,15 +62,14 @@ export function updatePlaneAxis(x, y, z, planePosition, camera) {
 
 
   // plane position & velocity
-  let turboSpeed = 0;
   if (controls.shift) {
     turbo += 0.025;
   } else {
     turbo *= 0.95;
   }
   turbo = Math.min(Math.max(turbo, 0), 1);
-  // turboSpeed = Math.log2(turbo + 1) * 0.01;
-  turboSpeed = easeOutQuad(turbo) * 0.02;
+
+  let turboSpeed = easeOutQuad(turbo) * 0.02;
 
   camera.fov = 45 + turboSpeed * 1500;
   camera.updateProjectionMatrix();
