def3af0681bd44afcbf37ed2d19799551b385219
Domenicobrz <domenicobrz@gmail.com>
2023-07-29 08:13:38 +0200

    removed unused assets

--- src/SphereEnv.jsx
+++ src/SphereEnv.jsx
@@ -2,7 +2,7 @@ import { useTexture } from "@react-three/drei";
 import { BackSide } from "three";
 
 export function SphereEnv() {
-  const map = useTexture("assets/textures/envmap4.jpg");
+  const map = useTexture("assets/textures/envmap.jpg");
 
   return <mesh>
     <sphereGeometry args={[60, 50, 50]} />
