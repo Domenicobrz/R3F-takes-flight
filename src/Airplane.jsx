30fd7e83482813cc1322079a468260f34deef966
Domenicobrz <domenicobrz@gmail.com>
2023-08-12 22:49:33 +0200

    ep 3

--- src/Airplane.jsx
+++ src/Airplane.jsx
@@ -5,10 +5,17 @@ Command: npx gltfjsx@6.2.7 public/assets/models/airplane.glb
 
 import React, { useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
-import { Matrix4, Vector3 } from 'three';
+import { Matrix4, Quaternion, Vector3 } from 'three';
 import { useFrame } from '@react-three/fiber';
+import { updatePlaneAxis } from './controls';
 
+const x = new Vector3(1, 0, 0);
+const y = new Vector3(0, 1, 0);
+const z = new Vector3(0, 0, 1);
 export const planePosition = new Vector3(0, 3, 7);
+
+const delayedRotMatrix = new Matrix4();
+const delayedQuaternion = new Quaternion();
 
 export function Airplane(props) {
   // thanks to:
@@ -18,17 +25,35 @@ export function Airplane(props) {
   const helixMeshRef = useRef();
 
   useFrame(({ camera }) => {
-    planePosition.add(new Vector3(0, 0, -0.005));
-    
+    updatePlaneAxis(x, y, z, planePosition, camera);
+
+    const rotMatrix = new Matrix4().makeBasis(x, y, z);
+
     const matrix = new Matrix4()
-      .multiply(new Matrix4().makeTranslation(planePosition.x, planePosition.y, planePosition.z));
+      .multiply(new Matrix4().makeTranslation(planePosition.x, planePosition.y, planePosition.z))
+      .multiply(rotMatrix);
 
     groupRef.current.matrixAutoUpdate = false;
     groupRef.current.matrix.copy(matrix);
     groupRef.current.matrixWorldNeedsUpdate = true;
 
+
+    var quaternionA = new Quaternion().copy(delayedQuaternion);
+    var quaternionB = new Quaternion();
+    quaternionB.setFromRotationMatrix(rotMatrix);
+
+    var interpolationFactor = 0.175;
+    var interpolatedQuaternion = new Quaternion().copy(quaternionA);
+    interpolatedQuaternion.slerp(quaternionB, interpolationFactor);
+    delayedQuaternion.copy(interpolatedQuaternion);
+
+    delayedRotMatrix.identity();
+    delayedRotMatrix.makeRotationFromQuaternion(delayedQuaternion);
+
+
     const cameraMatrix = new Matrix4()
       .multiply(new Matrix4().makeTranslation(planePosition.x, planePosition.y, planePosition.z))
+      .multiply(delayedRotMatrix)
       .multiply(new Matrix4().makeRotationX(-0.2))
       .multiply(
         new Matrix4().makeTranslation(0, 0.015, 0.3)
